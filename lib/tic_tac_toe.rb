cc58905c42832455144b69628258a4c50519ea95
Michael McGovern <mmcgovern.wework@gmail.com>
2019-03-14 16:12:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,11 +50,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0 
-    "X"
-  else
-    "O"
-  end
+  turn_count(board).even? ? 'X' : 'O'
 end
 
 def won?(board)
